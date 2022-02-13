617cb0095d755054e8388196500f77fc536992ed
brianjrok <brianjrok@gmail.com>
2022-02-13 18:15:35 -0500

    minor layout tweaks and additions

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const links = document.getElementsByTagName("li");
+const pages = document.getElementsByClassName("content");
 
 for (var i = 0; i < links.length; i++) {
   links[i].addEventListener("click", function (e) {
-    console.log(e.target.innerHTML);
+    activateLink(e.target);
   });
+}
+
+function activateLink(link) {
+  for (var i = 0; i < pages.length; i++) {
+    if (pages[i].id == link.innerHTML.toLowerCase()) {
+      pages[i].setAttribute("data-active", "true");
+    } else {
+      pages[i].setAttribute("data-active", "false")
+    }
+  }
 }
